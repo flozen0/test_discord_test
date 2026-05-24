50bd6856542dace0c4c77afd1dd66e92670e3633
flozen0 <flozen0be@gmail.com>
2026-05-24 19:26:52 +0200

    Update index.js
    less spam

--- index.js
+++ index.js
@@ -17,21 +17,48 @@ client.on('ready', () => {
 
 client.on('voiceStateUpdate', async (oldState, newState) => {
 
-    console.log("EVENT DETECTE");
+    // quelqu'un rejoint un salon
+    if (oldState.channelId !== newState.channelId && newState.channel) {
 
-    // rejoint un vocal
-    if (!oldState.channel && newState.channel) {
+        const channel = newState.channel;
 
-        console.log("JOIN DETECTE");
+        // nombre de personnes dans le vocal
+        const memberCount = channel.members.size;
 
         try {
 
-            await axios.post(WEBHOOK_URL, {
-                content:
-                `🔊 ${newState.member.user.username} a rejoint ${newState.channel.name}`
-            });
+            // PREMIERE PERSONNE
+            if (memberCount === 1) {
 
-            console.log("MESSAGE ENVOYE");
+                await axios.post(WEBHOOK_URL, {
+                    content:
+                    `🔊 ${newState.member.user.username} a lancé une conversation dans ${channel.name}`
+                });
+
+                console.log("Premier membre détecté");
+            }
+
+            // 5 PERSONNES
+            if (memberCount >= 5) {
+
+                const now = Date.now();
+
+                // cooldown 24h
+                if (
+                    !lastPing[channel.id] ||
+                    now - lastPing[channel.id] > 24 * 60 * 60 * 1000
+                ) {
+
+                    lastPing[channel.id] = now;
+
+                    await axios.post(WEBHOOK_URL, {
+                        content:
+                        `@everyone 🔥 Il y a ${memberCount} personnes dans ${channel.name}, whole house mad !`
+                    });
+
+                    console.log("@everyone envoyé");
+                }
+            }
 
         } catch (err) {
             console.error(err);
