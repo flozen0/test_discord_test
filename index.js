73915789b70f1f87b15a2830dcee2ab42a9ffe2c
flozen0 <flozen0be@gmail.com>
2026-05-24 12:29:39 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const axios = require('axios');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates
+    ]
+});
+
+const TOKEN = process.env.TOKEN;
+const WEBHOOK_URL = process.env.WEBHOOK_URL;
+
+client.on('ready', () => {
+    console.log(`Connecté en tant que ${client.user.tag}`);
+});
+
+client.on('voiceStateUpdate', async (oldState, newState) => {
+
+    // rejoint un vocal
+    if (!oldState.channel && newState.channel) {
+
+        try {
+
+            await axios.post(WEBHOOK_URL, {
+                content:
+                `🔊 ${newState.member.user.username} a rejoint ${newState.channel.name}`
+            });
+
+        } catch (err) {
+            console.error(err);
+        }
+    }
+});
+
+client.login(TOKEN);
